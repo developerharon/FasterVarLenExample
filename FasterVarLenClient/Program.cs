cd1b310e630d470507a088a1c08520ede1d97ea5
Haron Kipkorir <ronoharonkip@gmail.com>
2022-06-08 19:47:27 +0300

    initial commit

---
+++ FasterVarLenClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using FASTER.client;
+
+Console.WriteLine("FASTER variable-length KV client");
+
+using var client = new FasterKVClient<Student, Student>("127.0.0.1", 5000);
+
+var session = client.NewSession(new StudentFunctions(), FASTER.common.WireFormat.DefaultVarLenKV);
+
+
+internal class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Course { get; set; }
+
+    public Student(string name, string course)
+    {
+        Name = name;
+        Course = course;
+    }
+}
+
+internal class StudentFunctions : CallbackFunctionsBase<Student, Student, Student, Student, byte>
+{
+    public override void ReadCompletionCallback(ref Student key, ref Student input, ref Student output, byte ctx, Status status)
+    {
+        base.ReadCompletionCallback(ref key, ref input, ref output, ctx, status);
+    }
+}
---
+++ FasterVarLenServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using FASTER.server;
+
+Console.WriteLine("FASTER variable-length KV server");
+
+var serverOptions = new ServerOptions()
+{
+    Port = 5000,
+    Address = "127.0.0.1",
+};
+
+using var server = new VarLenServer(serverOptions);
+server.Start();
+
+Console.WriteLine("Server started.");
+
+Thread.Sleep(Timeout.Infinite);
