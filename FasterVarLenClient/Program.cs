ac248d83d8a84b455d43ad36b85b0183078381cd
Haron Kipkorir <ronoharonkip@gmail.com>
2022-06-14 22:55:06 +0300

    A simple client-server model working

--- FasterVarLenClient/Program.cs
+++ FasterVarLenClient/Program.cs
@@ -1,29 +1,107 @@
 ﻿using FASTER.client;
-
-Console.WriteLine("FASTER variable-length KV client");
-
-using var client = new FasterKVClient<Student, Student>("127.0.0.1", 5000);
-
-var session = client.NewSession(new StudentFunctions(), FASTER.common.WireFormat.DefaultVarLenKV);
+using System.Buffers;
+using System.Text;
+using System.Text.Json;
 
 
-internal class Student
+TestClass.Run();
+
+internal static class TestClass
+{
+
+    private const string ip = "127.0.0.1";
+    private const int port = 5000;
+    private static Encoding encode = Encoding.UTF8;
+    private static ArrayPool<byte> pool = ArrayPool<byte>.Shared;
+
+
+    public static void Run()
+    {
+
+        using var client1 = new FasterKVClient<ReadOnlyMemory<byte>, ReadOnlyMemory<byte>>(ip, port);
+        using var session = client1.NewSession(new MemoryFunctions()); // uses protocol WireFormat.DefaultVarLenKV by default
+
+        SyncMemorySamples(session);
+    }
+    static void SyncMemorySamples(ClientSession<ReadOnlyMemory<byte>, ReadOnlyMemory<byte>, ReadOnlyMemory<byte>, (IMemoryOwner<byte>, int), byte, MemoryFunctions, MemoryParameterSerializer<byte>> session)
+    {
+        var student = new Student
+        {
+            Id = 3,
+            Name = "Emmanuel Changole",
+            Course = "Computer Science"
+        };
+
+        int idLength = encode.GetByteCount(student.Id.ToString());
+        int studentLength = encode.GetByteCount(JsonSerializer.Serialize(student));
+
+        byte[] idBytes = pool.Rent(idLength);
+        int bytesWritten = encode.GetBytes(student.Id.ToString(), idBytes);
+        var key = idBytes.AsMemory(0, bytesWritten);
+
+
+        byte[] studentBytes = pool.Rent(studentLength);
+        bytesWritten = encode.GetBytes(JsonSerializer.Serialize(student), studentBytes);
+        var value = studentBytes.AsMemory(0, bytesWritten);
+
+        session.Upsert(key, value);
+
+        // Flushes partially filled batches, does not wait for responses
+        session.Flush();
+
+        session.Read(key);
+        session.CompletePending(true);
+    }
+}
+
+public class MemoryFunctions : MemoryFunctionsBase<byte>
+{
+    public override void ReadCompletionCallback(ref ReadOnlyMemory<byte> key, ref ReadOnlyMemory<byte> input, ref (IMemoryOwner<byte>, int) output, byte ctx, Status status)
+    {
+        Memory<int> expected = new Memory<int>(new int[key.Span.Length]);
+
+        var student = JsonSerializer.Deserialize<Student>(output.Item1.Memory.Span.Slice(0, output.Item2), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        Console.WriteLine($"Output read: {student}");
+        //for (int i = 0; i < output.Item2; i++)
+        //{
+        //    Console.WriteLine($"Output read: {output.Item1.Memory.Span[i]}");
+        //}
+    }
+}
+
+public class MemoryFunctionsByte : MemoryFunctionsBase<byte>
+{
+    public override void ReadCompletionCallback(ref ReadOnlyMemory<byte> key, ref ReadOnlyMemory<byte> input, ref (IMemoryOwner<byte>, int) output, byte ctx, Status status)
+    {
+        Console.WriteLine($"{nameof(this.ReadCompletionCallback)} 2 called");
+        try
+        {
+            if (ctx == 0)
+            {
+                if (!status.Found || !key.Span.SequenceEqual(output.Item1.Memory.Span.Slice(0, output.Item2)))
+                    throw new Exception("Incorrect read result");
+            }
+            else
+            {
+                throw new Exception("Unexpected user context");
+            }
+        }
+        finally
+        {
+            output.Item1.Dispose();
+        }
+    }
+}
+
+
+public class Student
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Course { get; set; }
 
-    public Student(string name, string course)
+    public override string ToString()
     {
-        Name = name;
-        Course = course;
-    }
-}
-
-internal class StudentFunctions : CallbackFunctionsBase<Student, Student, Student, Student, byte>
-{
-    public override void ReadCompletionCallback(ref Student key, ref Student input, ref Student output, byte ctx, Status status)
-    {
-        base.ReadCompletionCallback(ref key, ref input, ref output, ctx, status);
+        return $"\nId: {Id}\nName: {Name}\nCourse: {Course}";
     }
 }
--- FasterVarLenServer/Program.cs
+++ FasterVarLenServer/Program.cs
@@ -2,14 +2,27 @@
 
 Console.WriteLine("FASTER variable-length KV server");
 
+var path = Path.GetTempPath() + "faster/";
+
 var serverOptions = new ServerOptions()
 {
     Port = 5000,
     Address = "127.0.0.1",
+    MemorySize = "16g",
+    PageSize = "32m",
+    SegmentSize = "1g",
+    IndexSize = "8g",
+    EnableStorageTier = false,
+    LogDir = null,
+    CheckpointDir = null,
+    Recover = false,
+    DisablePubSub = false,
+    PubSubPageSize = "4k"
 };
 
 using var server = new VarLenServer(serverOptions);
 server.Start();
+
 
 Console.WriteLine("Server started.");
 
